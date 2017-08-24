bb9e06f794530ef9c18869c1de5bc0db5312fe30
Alex Dacre <a.dacre@thinkbits.com>
2017-08-24 13:02:31 +1000

    increased size of data transferred

--- user-files/simulations/publift/WebSocketStressSimulation.scala
+++ user-files/simulations/publift/WebSocketStressSimulation.scala
@@ -1,5 +1,6 @@
 package publift
 
+import java.util.UUID
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
@@ -15,12 +16,14 @@ class WebSocketStressSimulation extends Simulation {
     .userAgentHeader("Gatling2")
     .wsBaseURL("ws://localhost:3000")
 
+  val randomUser = UUID.randomUUID.toString
+
   val scn = scenario("WebSocket")
     .exec(ws("Connect").open("/"))
-    .exec(ws("Page Load").sendText("""{"type": "page_load"}"""))
-    .exec(ws("Bid Requests").sendText("""{"type": "bid_requests"}"""))
-    .exec(ws("Bid Results").sendText("""{"type": "bid_results"}"""))
-    .exec(ws("Creative Render").sendText("""{"type": "creative_render"}"""))
+    .exec(ws("Page Load").sendText(s"""{"type": "page_load", "value": {"event_type": "page_load", "user_id": "${randomUser}"}}"""))
+    .exec(ws("Bid Requests").sendText(s"""{"type": "bid_requests", "value": {"event_type": "bid_requests", "user_id": "${randomUser}"}}"""))
+    .exec(ws("Bid Results").sendText(s"""{"type": "bid_results", "value": {"event_type": "bid_results", "user_id": "${randomUser}"}}"""))
+    .exec(ws("Creative Render").sendText(s"""{"type": "creative_render", "value": {"event_type": "creative_render", "user_id": "${randomUser}"}}"""))
 
-  setUp(scn.inject(rampUsers(25000) over (10 seconds))).protocols(httpConf)
+  setUp(scn.inject(rampUsers(10000) over (10 seconds))).protocols(httpConf)
 }
